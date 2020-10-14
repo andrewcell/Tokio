f9ec5b4ca0d033c47e113b3e79aeaebb42428435
Andrew M. Bray <a@vxz.me>
2020-10-14 15:33:15 +0900

    Recent updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+    input: string;
+}
+
+class App extends React.Component<unknown, AppState> {
+    constructor(props: unknown) {
+        super(props);
+        this.state = {
+            input: ''
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown = (ev: KeyboardEvent) => {
+        // alert(ev.code)
+        if (ev.key.length <= 1) {
+            this.setState(prevState => {
+                return {input: prevState.input + ev.key}
+            })
+        }
+    }
+
+    render(): React.ReactElement {
+        const { input } = this.state;
+        return (
+            <div className="App">
+                <header>
+                </header>
+                <section>
+                    <p>Microsoft Windows [Version 10.0.10240]</p>
+                    <p>&copy; Microsoft Corporation. All rights reserved.</p>
+                    <br />
+                    <div id={'input'}>
+                        <p>C:\Windows\System32></p>
+                        <p>{input}</p>
+                    </div>
+
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
